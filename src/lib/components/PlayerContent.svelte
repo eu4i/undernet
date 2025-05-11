c73db975c530144a158fed706716847a00bc24f2
eu4i <t.cedervall@pm.me>
2025-05-11 19:12:58 +0200

    downstripping

--- src/lib/components/PlayerContent.svelte
+++ src/lib/components/PlayerContent.svelte
@@ -1,11 +1,9 @@
 <script>
-  import CDSlider from '$lib/components/CDSlider.svelte';
 </script>
 
 <div class="window" id="video-container"></div>
 <div class="lower-music-content">
   <div id="cdslider-container">
-    <CDSlider />
   </div>
   <div class="window" id="track-container"></div>
 </div>
